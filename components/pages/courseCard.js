23614074ad757f947ce3a2107ff2095025ef1fb3
SulaimanovU <uliana.sulaimanov@bk.ru>
2022-02-12 10:24:34 +0600

    feat: turned course cards to component

---
+++ components/pages/courseCard.js
@@ -0,0 +1,104 @@
+
+export const Coursecard = () => {
+  return (
+    <div className="cousers__cards--container">
+      <div className="cousers--card">
+        <div style={{ background: "linear-gradient(333.43deg,#00ecb3 16.67%,#9349f0)" }} className="card--preview">
+          <i className="fab fa-react"></i>
+            <div className="card--title">Frontend разработка <br /> JavaScript/React.js</div>
+            <div className="card--title card-sub-title">7000 сом/мес.</div>
+          </div>
+          <div className="card--description">
+            <div className="description">
+              <strong>Длительность: </strong>
+              <p>6 месяцев</p>
+            </div>
+            <div className="description">
+              <strong>Дата запуска: </strong>
+              <p>20 октября</p>
+            </div>
+            <div className="description">
+              <strong>Дни уроков: </strong>
+              <p>Вт-Чт-Сб</p>
+            </div>
+            <div className="description">
+              <strong>Время уроков: </strong>
+              <p>18:00 - 20:00</p>
+            </div>
+            <div className="description">
+              <strong>Осталось: </strong>
+              <p>5 мест</p>
+            </div>
+          </div>
+            <div className="card--more">
+              <a href="/react.html" className="btn btn-blue animate-y">Подробнее</a>
+            </div>
+          </div>
+          <div className="cousers--card">
+            <div style={{ background: "linear-gradient(153.43deg,#ffe814,#e51686 83.33%)" }} className="card--preview">
+              <i className="fab fa-python"></i>
+              <div className="card--title">Frontend разработка <br /> JavaScript/React.js</div>
+              <div className="card--title card-sub-title">7000 сом/мес.</div>
+            </div>
+            <div className="card--description">
+            <div className="description">
+              <strong>Длительность: </strong>
+              <p>6 месяцев</p>
+            </div>
+            <div className="description">
+              <strong>Дата запуска: </strong>
+              <p>20 октября</p>
+            </div>
+            <div className="description">
+              <strong>Дни уроков: </strong>
+              <p>Вт-Чт-Сб</p>
+            </div>
+            <div className="description">
+              <strong>Время уроков: </strong>
+              <p>18:00 - 20:00</p>
+            </div>
+            <div className="description">
+              <strong>Осталось: </strong>
+              <p>5 мест</p>
+            </div>
+          </div>
+          <div className="card--more">
+            <a href="/react.html" className="btn btn-blue animate-y">Подробнее</a>
+          </div>
+        </div>
+        <div className="cousers--card">
+          <div className="card--preview">
+            <i className="fab fa-node-js"></i>
+            <div className="card--title">Frontend разработка <br /> JavaScript/React.js</div>
+            <div className="card--title card-sub-title">7000 сом/мес.</div>
+          </div>
+          <div className="card--description">
+            <div className="description">
+              <strong>Длительность: </strong>
+              <p>6 месяцев</p>
+            </div>
+            <div className="description">
+              <strong>Дата запуска: </strong>
+              <p>20 октября</p>
+            </div>
+            <div className="description">
+              <strong>Дни уроков: </strong>
+              <p>Вт-Чт-Сб</p>
+            </div>
+            <div className="description">
+              <strong>Время уроков: </strong>
+              <p>18:00 - 20:00</p>
+            </div>
+            <div className="description">
+              <strong>Осталось: </strong>
+              <p>5 мест</p>
+            </div>
+          </div>
+          <div className="card--more">
+            <a href="/react.html" className="btn btn-blue animate-y">Подробнее</a>
+          </div>
+        </div>
+      </div>
+    );
+}
+
