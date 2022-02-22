5083a611dc45ddc6a71134cc87f33382a66cfab0
Esenushka <prosterling.mien@gmail.com>
2022-02-23 01:19:32 +0600

    feat:add mentors-integretion,made object-fitto mentors img

--- components/common/mentors/mentorsCard.jsx
+++ components/common/mentors/mentorsCard.jsx
@@ -1,14 +1,14 @@
 
-export const Mentorscard = () => {
+export const Mentorscard = ({ img, name, info}) => {
   return (
     <>
       <div className="col-md-4">
-        <div className="mt-3">
+        <div className="mt-3 text-center">
           <img data-src="images/team/Ilyas.png" alt="Ilyas"
-            className="lazy img-fluid mx-auto d-block rounded img-thumbnail loaded" src="images/me2.jpg" />
+            className="lazy img-fluid mx-auto d-block rounded img-thumbnail loaded" src={img} />
           <div className="text-center mt-3">
-            <p className="font-weight-bold mb-0">Асылбеков Аманбек</p>
-            <p className="text-muted mb-0">CEO/Основатель</p>
+            <p className="font-weight-bold mb-0">{name}</p>
+            <p className="text-muted mb-0">{info}</p>
           </div>
         </div>
       </div>
