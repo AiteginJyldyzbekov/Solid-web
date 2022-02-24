01304f003139152daefe80bc3c3e3a55b8ecb082
Esenushka <prosterling.mien@gmail.com>
2022-02-24 16:54:22 +0600

    fix again HomePage

--- components/pages/HomePage.js
+++ components/pages/HomePage.js
@@ -29,7 +29,7 @@ export default function HomePage() {
       querySnapshot.forEach((doc) => {
         courses.push({...doc.data(), id: doc.id})
         setCourses(courses)
-
+      })
       });
     db.collection("mentors")
       .get()
@@ -93,72 +93,7 @@ export default function HomePage() {
               </div>
               <div className="about-parallax-bg"></div>
             </div>
-          </div>
-        </main>
-        <section className="courses container">
-          <h2 className="courses-title">Ближайшие курсы</h2>
-          <div className="header-subtitle">
-            Выбери себе подходящий курс и стань программистом в следующих <br />
-            направлениях
-          </div>
-          <div className="cousers__cards--container">
-            {course.map((item) => (
-              <Coursecard
-                {...item}
-                key={item.id}
-                leftColor={item.leftColor}
-                rightColor={item.rightColor}
-              />
-            ))}
-          </div>
-        </section>
-        <section id="app" className="app container">
-          <h2 className="courses-title">Почему выгодно обучаться у нас</h2>
-          <div className="header-subtitle">
-            Причины, по которым люди обучаются в нашей школе программирования
-          </div>
-          <div className="timeline-page mt-5">
-            {timeLine.map((item, index) => (
-              <Timeline key={item.id} isOdd={(index + 1) % 2 === 0} {...item} />
-            ))}
-          </div>
-        </section>
-        <section className="features container">
-          <h2 className="courses-title">Для кого наши курсы</h2>
-          <div className="header-subtitle">
-            Наши курсы для тебя, если ты хочешь:
-          </div>
-          <div className="row mt-5">
-            {[1, 2, 3].map((index) => (
-              <Courses key={index} />
-            ))}
-          </div>
-          <br />
-          <br />
-          <div className="header-btn">
-            <a id="write_us" className="btn btn-blue animate-y write_us">
-              Запишите меня на ваши курсы!
-            </a>
-          </div>
-        </section>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <section className="container about">
-          <h2 className="courses-title">
-            Мы не просто школа программирования, мы <br /> академия с глобальной
-            целью
-            <span style={{ color: "#0accda", lineHeight: "40px" }}>
-              стать фабрикой <br /> крутых программистов!
-            </span>
-          </h2>
-          <div className="row mt-5">
-            {mentor.map((item) => (
-              <Mentorscard {...item} key={item.id} />
-            ))}
-          </div>
+         
         </div>
       </main>
       <section className="courses container">
@@ -207,21 +142,34 @@ export default function HomePage() {
         </h2>
         <div className="row mt-5">
           {
-            [1, 2, 3].map((index) => <Mentorscard key={index} />)
+            mentor.map((item) => <Mentorscard {...item} key={item.id} />)
           }
         </div>
-      </section>
-      <section className="section bg-image__section jarallax" data-jarallax="{&quot;speed&quot;: 0.2}"
-        style={{ backgroundImage: "url(https://bitlab.kz/images/75.jpg)", filter: "none" }}>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-12">
-              <div className="text-center cta-Landsay mx-auto text-white">
-                <p className="cta-title mt-4 mb-4 font-weight-normal">Знания - это актив, который невозможно потерять.</p>
-                <div className="mt-4">
-                  <a className="btn btn-outline-custom btn-round font-weight-bold write_us" data-toggle="modal"
-                    data-target="#exampleModalLong">Получить знания</a>
-
+        </section>
+        <section
+          className="section bg-image__section jarallax"
+          data-jarallax='{"speed": 0.2}'
+          style={{
+            backgroundImage: "url(https://bitlab.kz/images/75.jpg)",
+            filter: "none",
+          }}
+        >
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-12">
+                <div className="text-center cta-Landsay mx-auto text-white">
+                  <p className="cta-title mt-4 mb-4 font-weight-normal">
+                    Знания - это актив, который невозможно потерять.
+                  </p>
+                  <div className="mt-4">
+                    <a
+                      className="btn btn-outline-custom btn-round font-weight-bold write_us"
+                      data-toggle="modal"
+                      data-target="#exampleModalLong"
+                    >
+                      Получить знания
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
