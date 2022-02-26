77d80f36c80cdfa6c9894a3d3ad82e1c8a4b4c5e
Esenushka <prosterling.mien@gmail.com>
2022-02-26 19:09:40 +0600

    feat:add free-advice-modal-window

--- components/pages/HomePage.js
+++ components/pages/HomePage.js
@@ -190,19 +190,24 @@ export default function HomePage() {
         <div onClick={OpenModal} className="modal-overlay"></div>
         <div className="modal">
           <div className="modal-header">
-            <h1>Solid Academy</h1>
+            <h1>Записаться на курсы</h1>
             <div onClick={OpenModal} className="modal-closeBtn">
             <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" clipRule="evenodd" d="M9.84606 12.4986L0.552631 3.20519C-0.1806 2.47196 -0.1806 1.28315 0.552631 0.549923C1.28586 -0.183308 2.47466 -0.183308 3.20789 0.549923L12.5013 9.84335L21.792 0.552631C22.5253 -0.1806 23.7141 -0.1806 24.4473 0.552631C25.1805 1.28586 25.1805 2.47466 24.4473 3.20789L15.1566 12.4986L24.45 21.792C25.1832 22.5253 25.1832 23.7141 24.45 24.4473C23.7168 25.1805 22.528 25.1805 21.7947 24.4473L12.5013 15.1539L3.20519 24.45C2.47196 25.1832 1.28315 25.1832 0.549923 24.45C-0.183308 23.7168 -0.183308 22.528 0.549923 21.7947L9.84606 12.4986Z" fill="blue"></path>
+            <path fillRule="evenodd" clipRule="evenodd" d="M9.84606 12.4986L0.552631 3.20519C-0.1806 2.47196 -0.1806 1.28315 0.552631 0.549923C1.28586 -0.183308 2.47466 -0.183308 3.20789 0.549923L12.5013 9.84335L21.792 0.552631C22.5253 -0.1806 23.7141 -0.1806 24.4473 0.552631C25.1805 1.28586 25.1805 2.47466 24.4473 3.20789L15.1566 12.4986L24.45 21.792C25.1832 22.5253 25.1832 23.7141 24.45 24.4473C23.7168 25.1805 22.528 25.1805 21.7947 24.4473L12.5013 15.1539L3.20519 24.45C2.47196 25.1832 1.28315 25.1832 0.549923 24.45C-0.183308 23.7168 -0.183308 22.528 0.549923 21.7947L9.84606 12.4986Z" fill="gray"></path>
             </svg>
             </div>
-            
           </div>
           <form onSubmit={submit} className="modal__contact__form">
-             <input value={user} onChange={(e)=> setUser(e.target.value)} placeholder="Имя"/>
-             <input value={number} onChange={(e)=> setNumber(e.target.value)} placeholder="Номер"/>
-             <div>
-               <button className="btn btn-blue  modal--btn">Напишите нам</button>
+            <div>
+            <label>ИМЯ</label>
+             <input type='text' value={user} onChange={(e)=> setUser(e.target.value)} placeholder="Эсенбек"/>
+            </div>
+            <div>
+            <label>НОМЕР</label>
+            <input type='text' value={number} onChange={(e)=> setNumber(e.target.value)} placeholder="+996 707 62 96 17"/>
+            </div>
+             <div className="modal-btn-wrapper">
+               <button className="btn  modal--btn">Напишите нам</button>
              </div>
             </form>
         </div>
