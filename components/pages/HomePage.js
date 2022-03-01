250dd3032381af5c57dbc421872cc7a630a69e98
SulaimanovU <uliana.sulaimanov@bk.ru>
2022-03-01 18:12:46 +0600

    feat: integration for detailPage

--- components/pages/HomePage.js
+++ components/pages/HomePage.js
@@ -16,6 +16,7 @@ export default function HomePage() {
     return () => window.removeEventListener("scroll", handleScroll);
   });
 
+
   let whostudy = [];
   let courses = [];
   let timeLineItem = [];
