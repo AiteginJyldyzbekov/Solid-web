4edf172a77919139ed9950034f8dabb48a752bf6
Esenushka <prosterling.mien@gmail.com>
2022-07-30 21:45:27 +0600

    fix:preloader and adaptive on mobile

--- components/common/header/Header.js
+++ components/common/header/Header.js
@@ -26,12 +26,6 @@ export default function Header() {
             <a href="tel:+996707629617" className="nav-link btn btn-brighred animate-y ">Позвонить</a>
           </li>
         </ul>
-        <div className="menu-bars" id="toggle">
-          <input type="checkbox" id="checkbox" />
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
       </div>
     </div>
   </nav>;
