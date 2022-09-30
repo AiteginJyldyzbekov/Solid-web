ef6d8255e6f5b355a8866945d5ef00883775b4e5
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-09-30 17:01:23 +0600

    fix:added margin between mentors img and text

--- components/common/mentors/mentorsCard.jsx
+++ components/common/mentors/mentorsCard.jsx
@@ -4,7 +4,7 @@ export const Mentorscard = ({ img, name, info}) => {
       <div className="col-md-4">
         <div className="mt-3 text-center">
           <img data-src="images/team/Ilyas.png" alt="Ilyas"
-            className="lazy img-fluid mx-auto d-block rounded img-thumbnail loaded mb-5" src={img} />
+            className="lazy img-fluid mx-auto d-block rounded img-thumbnail loaded" src={img} />
           <div className="text-center mt-3">
             <p className="font-weight-bold mb-0">{name}</p>
             <p className="text-muted mb-0">{info}</p>
