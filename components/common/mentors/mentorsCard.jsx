84b94197784cc181c8cfe0672a0d60e022ef7fd8
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-09-30 18:31:46 +0600

    fix: mentors card margins

--- components/common/mentors/mentorsCard.jsx
+++ components/common/mentors/mentorsCard.jsx
@@ -2,7 +2,7 @@ export const Mentorscard = ({ img, name, info}) => {
   return (
     <>
       <div className="col-md-4">
-        <div className="mt-3 text-center">
+        <div className="mt-5 text-center">
           <img data-src="images/team/Ilyas.png" alt="Ilyas"
             className="lazy img-fluid mx-auto d-block rounded img-thumbnail loaded" src={img} />
           <div className="text-center mt-3">
