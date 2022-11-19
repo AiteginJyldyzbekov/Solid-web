7d6f5840314ef29b92aef84889d08ecb904d7b23
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2022-11-19 21:55:34 +0600

    feat: add ananlitics scripts

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,19 @@ import firebase from "../config/firebase.js";
 import WithAuth from "../hoks/privateAuth.js";
 
 function MyApp({ Component, pageProps }) {
+  if (typeof window === "object") {
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-TPKSSBV");
+  }
+
   const { pathname } = useRouter();
   const [isIncludeAdmin, setIsIncludeAdmin] = useState();
   const [isAuth, setIsAuth] = useState(false);
@@ -17,16 +30,16 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     firebase.auth().onAuthStateChanged(async (user) => {
       if (user) {
-        setIsAuth(user?.toJSON())
+        setIsAuth(user?.toJSON());
       } else {
-        setIsAuth(false)
+        setIsAuth(false);
       }
     });
   }, []);
 
   useEffect(() => {
     const arr = pathname.split("/");
-    const check = arr.includes("admin") && arr.length > 2
+    const check = arr.includes("admin") && arr.length > 2;
     setIsIncludeAdmin(check);
   }, [pathname]);
 
@@ -44,20 +57,23 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       {isIncludeAdmin || <Header />}
-      {
-        isIncludeAdmin
-          ? <WithAuth Component={
-            () => <Component
-              {...pageProps}
-              isAuth={isAuth}
-            />}
-            isAuth={isAuth}
-          />
-          : <Component
-            {...pageProps}
-            isAuth={isAuth}
-          />
-      }
+      {isIncludeAdmin ? (
+        <WithAuth
+          Component={() => <Component {...pageProps} isAuth={isAuth} />}
+          isAuth={isAuth}
+        />
+      ) : (
+        <Component {...pageProps} isAuth={isAuth} />
+      )}
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TPKSSBV"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
     </>
   );
 }
